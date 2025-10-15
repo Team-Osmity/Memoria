dbcf8373d7802f894acdd11fbc782334d40c6bf3
shizuku86 <shizuku86710@gmail.com>
2025-10-15 17:08:13 +0900

    deleted singleton

--- Assets/Codes/GameManager.cs
+++ Assets/Codes/GameManager.cs
@@ -2,19 +2,5 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public static GameManager Instance { get; private set; }
-
-    void Awake() {
-        makeInstance();
-    }
-
-    private void makeInstance() {
-        if (Instance == null) {
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else {
-            Destroy(gameObject);
-        }
-    }
+    
 }
