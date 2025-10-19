9d14b3fa01756ab01f3ede9037d41efc5634fbe7
shizuku86 <shizuku86710@gmail.com>
2025-10-20 00:04:25 +0900

    make ShowLoadingAsync()

---
+++ Assets/Project/Scripts/Constants/Scenes.cs
@@ -0,0 +1,20 @@
+namespace Memoria.Constants
+{
+    public static class Scenes
+    {
+        public enum ContentScene
+        {
+            Title,
+            MainMenu,
+            Stage01,
+            Ending
+        }
+
+        public enum OverlayScene
+        {
+            HUD,
+            PauseMenu,
+            Settings
+        }
+    }
+}
--- Assets/Project/Scripts/Systems/SceneDirector.cs
+++ Assets/Project/Scripts/Systems/SceneDirector.cs
@@ -2,18 +2,38 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Memoria.Constants;
 
 namespace Memoria.Systems
 {
     public class SceneDirector : MonoBehaviour
     {
         public static SceneDirector Instance { get; private set; }
+        [SerializeField] private CanvasGroup loadingOverlay;
+        private float loadingFadeDuration = 0.25f;
+
+        public bool isBusy { get; private set; }
 
         void Awake()
         {
             if (Instance && Instance != this) { Destroy(gameObject); return; }
             Instance = this;
             DontDestroyOnLoad(gameObject);
+        }
+
+        public async Task SwitchSceneAsync(Scenes.ContentScene nextScene, bool force = true)
+        {
+            if (isBusy && !force) return;
+            isBusy = true;
+
+            try
+            {
+                await ShowLoadingAsync();
+            }
+            finally
+            {
+                isBusy = false;
+            }
         }
     }
 }
