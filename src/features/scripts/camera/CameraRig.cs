ea563d841176c2b3d025c48e7f7e453df6e679ac
shizuku86 <shizuku86710@gmail.com>
2026-05-27 09:38:40 +0900

    added comments

--- src/features/scripts/camera/CameraRig.cs
+++ src/features/scripts/camera/CameraRig.cs
@@ -1,33 +1,23 @@
 using Godot;
 
-public partial class CameraRig
-    : Node3D
+// Camera に関する管理の責務を持つクラス
+public partial class CameraRig : Node3D
 {
-    private Node3D _target;
+    private Node3D _target; // Camera が中央に捉えるターゲットとなる Node を入れるための変数を用意しておく。基本的にはプレイヤーが入る前提。
+    private Node3D _yawPivot; // Camera　が横方向の視点の基準としてとらえる Node を入れるための変数を用意しておく
+    private Node3D _pitchPivot; // Camera が上下方向の視点の基準としてとらえる Node を入れるための変数を用意しておく
 
-    private Node3D _yawPivot;
-
-    private Node3D _pitchPivot;
-
-    private float _pitch;
-
-    public Basis CameraBasis =>
-        _yawPivot.GlobalBasis;
+    private float _pitch; // カメラの上下を度数法で保持するための変数を用意しておく
+    public Basis CameraBasis =>　_yawPivot.GlobalBasis; // Camera の回転情報を扱っている Basis 型の値を保持するための変数を用意しておく
 
     public override void _Ready()
     {
-        Input.MouseMode =
-            Input.MouseModeEnum.Captured;
-
-        _yawPivot =
-            GetNode<Node3D>(
-                "YawPivot");
-
-        _pitchPivot =
-            GetNode<Node3D>(
-                "YawPivot/PitchPivot");
+        Input.MouseMode = Input.MouseModeEnum.Captured;　// Godot が用意しているマウス制御用の設定を使って、マウスカーソルを固定して非表示にする。
+        _yawPivot = GetNode<Node3D>("YawPivot");　// Godot Editor で設定した Node を検索して取得し、変数に格納する。
+        _pitchPivot = GetNode<Node3D>("YawPivot/PitchPivot"); // Godot Editor で設定した Node を検索して取得し、変数の格納する。
     }
 
+    // 何かキー入力があった時に発火する Node3D が持っているメソッド
     public override void _Input(
         InputEvent e)
     {
